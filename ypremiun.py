960defedc3039faf4af08a18fcec533218e9d4a2
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-03-24 02:15:47 -0500

    Add files via upload

---
+++ ypremiun.py
@@ -0,0 +1,156 @@
+import random
+from random import randint
+import datetime
+from os import system
+system("clear")
+
+print("")
+print("\033[1;31m__   _______     ____ ___ _   _")
+print("\033[1;37m\ \ / /_   _|   | __ )_ _| \ | |")
+print("\033[1;31m \ V /  | |_____|  _ \| ||  \| |")
+print("\033[1;37m  | |   | |_____| |_) | || |\  |")
+print("\033[1;31m  |_|   |_|     |____/___|_| \_|")
+print("")
+print("          \033[1;33mV.1.2  \033[1;32m◎[\033[1;36m°‿\033[1;36m°\033[1;32m]◎")
+print("")
+print("\033[1;36m/* Nota: \033[1;37mIntenten asociar el bin")
+print("         \033[1;37ma Canva y Tilda") 
+print("")
+#VARIABLES
+a = (random.randrange(0,8,2))
+b = (random.randrange(0,8,2))
+c = (random.randrange(0,8,2))
+d = (random.randrange(0,8,2))
+
+#DATE
+def dategen():
+   now = datetime.datetime.now()
+   date = ""
+   month = str(randint(1, 9))
+   current_year = str(now.year)
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+
+print("")
+print("\033[1;33mL=O=A\033[1;34m=D=I=\033[1;31mN=G ...")
+system("sleep 3")
+print("")
+
+#IMPRIMIR
+print("\033[1;31m====Youtube Premiun====")
+print("")
+print(f"\033[1;34mBIN : \033[1;37m424094{a}{b}{c}{d}xxxxxx")
+print(f"\033[1;34mFECHA: \033[1;37m{dategen()}")
+print(f"\033[1;34mCVV: \033[1;37mgnd")
+print(f"\033[1;34mIP: \033[1;37mUSA")
+print("\n")
+
+#VARIABLES
+a = (random.randrange(0,8,2))
+b = (random.randrange(0,8,2))
+c = (random.randrange(0,8,2))
+d = (random.randrange(0,8,2))
+
+#DATE
+def dategen():
+   now = datetime.datetime.now()
+   date = ""
+   month = str(randint(1, 9))
+   current_year = str(now.year)
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+#IMPRIMIR
+print("\033[1;31m====Youtube Premiun====")
+print("")
+print(f"\033[1;34mBIN : \033[1;37m424094{a}{b}{c}{d}xxxxxx")
+print(f"\033[1;34mFECHA: \033[1;37m{dategen()}")
+print(f"\033[1;34mCVV: \033[1;37mgnd")
+print(f"\033[1;34mIP: \033[1;37mUSA")
+print("\n")
+
+#VARIABLES
+a = (random.randrange(0,8,2))
+b = (random.randrange(0,8,2))
+c = (random.randrange(0,8,2))
+d = (random.randrange(0,8,2))
+
+#DATE
+def dategen():
+   now = datetime.datetime.now()
+   date = ""
+   month = str(randint(1, 9))
+   current_year = str(now.year)
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+#IMPRIMIR
+print("\033[1;31m====Youtube Premiun====")
+print("")
+print(f"\033[1;34mBIN : \033[1;37m424094{a}{b}{c}{d}xxxxxx")
+print(f"\033[1;34mFECHA: \033[1;37m{dategen()}")
+print(f"\033[1;34mCVV: \033[1;37mgnd")
+print(f"\033[1;34mIP: \033[1;37mUSA")
+print("\n")
+
+#VARIABLES
+a = (random.randrange(0,8,2))
+b = (random.randrange(0,8,2))
+c = (random.randrange(0,8,2))
+d = (random.randrange(0,8,2))
+
+#DATE
+def dategen():
+   now = datetime.datetime.now()
+   date = ""
+   month = str(randint(1, 9))
+   current_year = str(now.year)
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+#IMPRIMIR
+print("\033[1;31m====Youtube Premiun====")
+print("")
+print(f"\033[1;34mBIN : \033[1;37m424094{a}{b}{c}{d}xxxxxx")
+print(f"\033[1;34mFECHA: \033[1;37m{dategen()}")
+print(f"\033[1;34mCVV: \033[1;37mgnd")
+print(f"\033[1;34mIP: \033[1;37mUSA")
+print("\n")
+
+#VARIABLES
+a = (random.randrange(0,8,2))
+b = (random.randrange(0,8,2))
+c = (random.randrange(0,8,2))
+d = (random.randrange(0,8,2))
+
+#DATE
+def dategen():
+   now = datetime.datetime.now()
+   date = ""
+   month = str(randint(1, 9))
+   current_year = str(now.year)
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+#IMPRIMIR
+print("\033[1;31m====Youtube Premiun====")
+print("")
+print(f"\033[1;34mBIN : \033[1;37m424094{a}{b}{c}{d}xxxxxx")
+print(f"\033[1;34mFECHA: \033[1;37m{dategen()}")
+print(f"\033[1;34mCVV: \033[1;37mgnd")
+print(f"\033[1;34mIP: \033[1;37mUSA")
+print("")
+print("\033[1;32m-> \033[1;37m'\033[1;33mPrender vpn para evitar baneo de IP\033[1;37m'")
+
+
